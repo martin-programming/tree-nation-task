7d4a6b4273a2cf478b409bd2f15c8ddcb5cbc15b
Martin Kolar <martin.kolar@aboutyou.com>
2026-06-11 11:13:13 +0200

    Implement docker-compose setup and update env variables

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "${SKIP_SETUP}" ]; then
+    if [ ! -f "vendor/autoload.php" ]; then
+        echo "==> Installing Composer dependencies..."
+        composer install --no-interaction --no-progress --optimize-autoloader
+    fi
+
+    if [ ! -f ".env" ]; then
+        echo "==> Copying .env.example to .env..."
+        cp .env.example .env
+        php artisan key:generate --force --ansi
+    fi
+
+    echo "==> Running database migrations..."
+    php artisan migrate --force --ansi
+fi
+
+exec "$@"
